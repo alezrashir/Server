0aac46eaca015f12856fffb1c935fcd67e7d01f2
shiralezra <shiralezra@gmail.com>
2017-11-14 12:43:12 +0200

    added name to fridge show

--- routes/fridge.js
+++ routes/fridge.js
@@ -23,23 +23,10 @@ router.get('/', function(req, res, next) {
 
     switch(queryData.function) {
         case "Show":
-            //var sql="SELECT * from fridges where fridgeid='" + queryData.fridgeid+"';SELECT * from users";
+          
 
 
-            //console.log(results[0]);
-          //  console.log(results[1]);
-          //   var sql= "SELECT actualitems.itemid ,actualvegetables.weight , actualvegetables.purchase , vegetables.expire from ((actualitems INNER JOIN actualvegetables ON   actualitems.fridgeid='" + queryData.fridgeid + "' AND actualitems.fridgeid=actualvegetables.fridgeid AND actualitems.itemid=actualvegetables.itemid)  INNER JOIN vegetables ON actualitems.itemid=vegetables.itemid);"+
-    // "  SELECT actualitems.itemid ,actualbarkod.expiredate , actualbarkod.quantity , items.imagepath from (((actualitems"+
-    //      "  INNER JOIN actualbarkod ON actualitems.fridgeid='66352618_D' AND  actualitems.fridgeid=actualbarkod.fridgeid AND actualitems.itemid=actualbarkod.itemid)"+
-    //     " INNER JOIN barkods ON actualitems.itemid=barkods.itemid)"+
-    //       " INNER JOIN items ON actualitems.itemid=items.itemid); SELECT actualitems.itemid ,actualvegetables.weight , actualvegetables.purchase , vegetables.expire from ((actualitems INNER JOIN actualvegetables ON   actualitems.fridgeid='" + queryData.fridgeid + "' AND actualitems.fridgeid=actualvegetables.fridgeid AND actualitems.itemid=actualvegetables.itemid)  INNER JOIN vegetables ON actualitems.iteactualitems.itemid=vegetables.itemid);" ;
-    //       //     , function(error, results, fields) {
-    //       //       if (error) {
-          //           throw error;
-          //       }
-
-
-            var sql = "SELECT   actualitems.type , actualitems.itemid ,actualvegetables.weight , actualvegetables.purchase , vegetables.expire , items.imagepath from (((actualitems INNER JOIN actualvegetables ON  actualitems.fridgeid='" + queryData.fridgeid +"' AND actualitems.fridgeid = actualvegetables.fridgeid AND actualitems.itemid = actualvegetables.itemid ) INNER JOIN vegetables ON actualitems.itemid = vegetables.itemid ) INNER JOIN items ON actualitems.itemid=items.itemid );  SELECT actualitems.type ,  actualitems.itemid ,actualbarkod.expiredate , actualbarkod.quantity , items.imagepath from (((actualitems INNER JOIN actualbarkod ON actualitems.fridgeid='" + queryData.fridgeid +"' AND  actualitems.fridgeid=actualbarkod.fridgeid AND actualitems.itemid=actualbarkod.itemid) INNER JOIN barkods ON actualitems.itemid=barkods.itemid) INNER JOIN items ON actualitems.itemid=items.itemid)";
+            var sql = "SELECT   actualitems.type , actualitems.itemid ,actualvegetables.weight , actualvegetables.purchase , vegetables.expire , items.imagepath ,items.name from (((actualitems INNER JOIN actualvegetables ON  actualitems.fridgeid='" + queryData.fridgeid +"' AND actualitems.fridgeid = actualvegetables.fridgeid AND actualitems.itemid = actualvegetables.itemid ) INNER JOIN vegetables ON actualitems.itemid = vegetables.itemid ) INNER JOIN items ON actualitems.itemid=items.itemid );  SELECT actualitems.type ,  actualitems.itemid ,actualbarkod.expiredate , actualbarkod.quantity , items.imagepath ,items.name from (((actualitems INNER JOIN actualbarkod ON actualitems.fridgeid='" + queryData.fridgeid +"' AND  actualitems.fridgeid=actualbarkod.fridgeid AND actualitems.itemid=actualbarkod.itemid) INNER JOIN barkods ON actualitems.itemid=barkods.itemid) INNER JOIN items ON actualitems.itemid=items.itemid)";
                // "SELECT actualitems.itemid ,actualbarkod.expiredate , actualbarkod.quantity , items.imagepathfrom (((actualitems INNER JOIN actualbarkod ON  actualitems.fridgeid='66352618_D' AND  actualitems.fridgeid=actualbarkod.fridgeid AND actualitems.itemid = actualbarkod.itemid ) INNER JOIN barkods ON actualitems.itemid=barkods.itemid) INNER JOIN items ON actualitems.itemid=items.itemid);";
 
             connection.query(sql,[2,1],function(error, results, fields) {
